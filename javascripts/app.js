80d7cb440a8c66ac61acf89e7e0f017efd1150dd
i-break-codes <vaibhav@browserstack.com>
2019-12-31 18:04:21 +0530

    remove jquery completely

--- javascripts/app.js
+++ javascripts/app.js
@@ -2,6 +2,7 @@ var App = function() {
   function init() {
     readMore();
     toggleMenu();
+    toggleTabs();
   }
 
   function readMore() {
@@ -34,6 +35,31 @@ var App = function() {
         getMenu.classList.add('hide');
         document.body.removeAttribute('style');
       }
+    });
+  }
+
+  function toggleTabs() {
+    var getTriggers = document.querySelectorAll('[data-trigger]');
+    getTriggers.forEach(function(el) {
+      el.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        var wrapper = el.closest('.resource-band'),
+        allNavs = wrapper.querySelectorAll('[data-trigger]');
+
+        allNavs.forEach(function(elm) {
+          elm.classList.remove('active-sub-tab');
+        });
+
+        el.classList.add('active-sub-tab');
+
+        var allScreens = wrapper.querySelectorAll('[data-target]');
+        allScreens.forEach(function(elScreen) {
+          elScreen.classList.add('hide');
+        });
+
+        wrapper.querySelector('[data-target="' + el.getAttribute('data-trigger') + '"]').classList.remove('hide');
+      });
     });
   }
 
